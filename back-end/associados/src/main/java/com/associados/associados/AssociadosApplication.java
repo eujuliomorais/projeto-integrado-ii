a1d974c76712049d0087e9352f4218b66efcd410
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2026-03-19 22:58:58 -0300

    chore: Spring Boot project set up

---
+++ back-end/associados/src/main/java/com/associados/associados/AssociadosApplication.java
@@ -0,0 +1,13 @@
+package com.associados.associados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociadosApplication.class, args);
+	}
+
+}
---
+++ back-end/associados/src/test/java/com/associados/associados/AssociadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.associados.associados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
