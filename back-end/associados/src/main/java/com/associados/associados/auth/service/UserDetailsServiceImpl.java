484ef7b4bf5c6426113aadf4d0494d2ec2e31081
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2026-04-27 22:02:52 -0300

    chore: rename parameter in loadUserByUsername method for clarity

--- back-end/associados/src/main/java/com/associados/associados/auth/service/UserDetailsServiceImpl.java
+++ back-end/associados/src/main/java/com/associados/associados/auth/service/UserDetailsServiceImpl.java
@@ -1,11 +1,12 @@
 package com.associados.associados.auth.service;
 
-import com.associados.associados.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.associados.associados.user.repository.UserRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -14,8 +15,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
     }
 }
