1fbcd3f2666c7f251cb580953f47c90c45661acf
Randson-Silva <randsonalves7@gmail.com>
2026-05-11 15:03:24 -0300

    bugfix(rebase): resolve rebase conflict between backend secConfiguration and sql file on develop

--- back-end/associados/src/main/java/com/associados/associados/auth/infra/security/SecurityConfiguration.java
+++ back-end/associados/src/main/java/com/associados/associados/auth/infra/security/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.associados.associados.auth.infra.security;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
